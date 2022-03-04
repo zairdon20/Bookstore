d33b7accaf7219d331a604b3bd87c76294cc5fa6
zairdon20 <josphat2020@gmail.com>
2022-03-04 14:47:55 +0200

    Add the functionality of storing, Add and remove

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import reportWebVitals from './reportWebVitals';
+import store from './redux/configureStore';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById('root'),
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
